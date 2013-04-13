b5e0f5d96e935885d9d3549bf424230a7a44c4c2
caok <caok1231@gmail.com>
2013-04-13 08:05:39 +0800

    update the code of angularjs

--- angularjs/app/assets/javascripts/tasks_controller.js
+++ angularjs/app/assets/javascripts/tasks_controller.js
@@ -11,3 +11,29 @@ function TasksCtrl($scope, $http) {
     //console.log(data);
   });
 }
+
+function FirstCtrl($scope) {
+}
+
+function SecondCtrl($scope) {
+  $scope.reversedMessage = function (message) {
+    return message.split("").reverse().join("");
+  }
+}
+
+//---------------------
+function PhoneListCtrl($scope) {
+  $scope.phones = [
+    {"name": "Nexus S",
+     "snippet": "Fast just got faster with Nexus S.",
+     "age": 0},
+    {"name": "Motorola XOOM™ with Wi-Fi",
+     "snippet": "The Next, Next Generation tablet.",
+     "age": 1},
+    {"name": "MOTOROLA XOOM™",
+     "snippet": "The Next, Next Generation tablet.",
+     "age": 2}
+  ];
+
+  $scope.orderProp = 'age';
+}
