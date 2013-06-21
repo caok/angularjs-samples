2f939b6fd8d62779b59ecec8775ffa268a3d69c4
caok <caok1231@gmail.com>
2013-06-21 13:39:39 +0800

    add query

--- angularjs/app/assets/angular/controllers/tasks_controller.js
+++ angularjs/app/assets/angular/controllers/tasks_controller.js
@@ -1,6 +1,7 @@
 function TasksCtrl($scope, $http) {
   $http.get('/tasks.json').success(function(data) {
     $scope.tasks = data;
+    $scope.orderProp = 'finished'
   });
 }
 
@@ -9,20 +10,3 @@ function TaskDetailCtrl($scope, $routeParams) {
   console.log($scope);
 }
 
-
-//---------------------
-//function PhoneListCtrl($scope) {
-  //$scope.phones = [
-    //{"name": "Nexus S",
-     //"snippet": "Fast just got faster with Nexus S.",
-     //"age": 0},
-    //{"name": "Motorola XOOM™ with Wi-Fi",
-     //"snippet": "The Next, Next Generation tablet.",
-     //"age": 1},
-    //{"name": "MOTOROLA XOOM™",
-     //"snippet": "The Next, Next Generation tablet.",
-     //"age": 2}
-  //];
-
-  //$scope.orderProp = 'age';
-//}
